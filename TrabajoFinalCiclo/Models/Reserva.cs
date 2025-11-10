be7b134c416cb144243de52d2978489700d6755e
MILTON ELI RIVERA MOLINA <RM12016@ti.ues.edu.sv>
2025-11-09 18:48:38 -0600

    Creando Clases Base (MODIFICACION 2)

---
+++ TrabajoFinalCiclo/Models/Reserva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoFinalCiclo.Models
+{
+    class Reserva
+    {
+        public int Id { get; set; }
+        public int SalaId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public int DuracionHoras { get; set; }
+        public List<string> Asistentes { get; set; }
+        public Dictionary<int, int> CombosPorPersona { get; set; }
+
+        public decimal CalcularCosto()
+        {
+            decimal total = 0;
+            foreach (var combo in CombosPorPersona)
+            {
+                decimal precio = combo.Key switch
+                {
+                    1 => 10,
+                    2 => 20,
+                    3 => 25,
+                    _ => 0
+                };
+                total += precio * combo.Value * DuracionHoras;
+            }
+            return total;
+        }
+}
